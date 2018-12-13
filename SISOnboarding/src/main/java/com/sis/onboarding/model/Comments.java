4c910c30c8fa9c487e562b2693115d60b1bf6f24
root <root@oc2626112146.ibm.com>
2018-12-13 14:19:20 +0530

    Commit it

---
+++ SISOnboarding/src/main/java/com/sis/onboarding/model/Comments.java
@@ -0,0 +1,29 @@
+package com.sis.onboarding.model;
+
+public class Comments {
+	
+	private String comments;
+	private String commentsDate;
+	private String commentsBy;
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	public String getCommentsDate() {
+		return commentsDate;
+	}
+	public void setCommentsDate(String commentsDate) {
+		this.commentsDate = commentsDate;
+	}
+	public String getCommentsBy() {
+		return commentsBy;
+	}
+	public void setCommentsBy(String commentsBy) {
+		this.commentsBy = commentsBy;
+	}
+	
+	
+
+}
